b0a18a6fdad5898bbedbfa31d0297fbc681e940b
xwen5 <xwen5@uci.edu>
2018-02-16 23:53:06 -0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -12,3 +12,7 @@ $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
+
+function reloadSelect(){
+    $('select').material_select();//will be called by dashboard.js after dynamically create selection.
+}
